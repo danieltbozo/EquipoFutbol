94026c71f086c5eb1528d53a650a8ab530c2c4ad
danieltbozo <38674997+danieltbozo@users.noreply.github.com>
2018-05-22 18:59:16 -0400

    Update DbInitializer.cs

--- EquipoFutbol1/EquipoFutbol1/Data/DbInitializer.cs
+++ EquipoFutbol1/EquipoFutbol1/Data/DbInitializer.cs
@@ -1,4 +1,4 @@
-﻿using EquipoFutbol1.Models;
+using EquipoFutbol1.Models;
 using System;
 using System.Linq;
 
@@ -18,15 +18,10 @@ namespace EquipoFutbol1.Data
 
             var equipos = new Equipo[]
             {
-            //new Equipo{IdEquipo=1, NombreEquipo="Argentina"},
-            //new Equipo{IdEquipo=2,NombreEquipo="Bolivia"},
-            //new Equipo{IdEquipo=3,NombreEquipo="Brasil"},
-            //new Equipo{IdEquipo=4,NombreEquipo="Alemania"},
-
-             new Equipo{NombreEquipo="Argentina"},
-             new Equipo{NombreEquipo="Bolivia"},
-             new Equipo{NombreEquipo="Brasil"},
-             new Equipo{NombreEquipo="Alemania"},
+              new Equipo{NombreEquipo="Argentina"},
+              new Equipo{NombreEquipo="Bolivia"},
+              new Equipo{NombreEquipo="Brasil"},
+              new Equipo{NombreEquipo="Alemania"},
             };
             foreach (var e in equipos)
             {
@@ -36,12 +31,27 @@ namespace EquipoFutbol1.Data
 
             var jugadores = new Jugador[]
             {
-            new Jugador{IdJugador=1, NombreJugador="Jugador1Eq1", Edad=20, IdEquipo=1},
-            new Jugador{IdJugador=2, NombreJugador="Jugador2Eq1", Edad=21, IdEquipo=1},
-            new Jugador{IdJugador=3, NombreJugador="Jugador3Eq1", Edad=20, IdEquipo=1},
-            new Jugador{IdJugador=4, NombreJugador="Jugador1Eq2", Edad=20, IdEquipo=2},
-            new Jugador{IdJugador=5, NombreJugador="Jugador2Eq2", Edad=21, IdEquipo=2},
-            new Jugador{IdJugador=6, NombreJugador="Jugador3Eq2", Edad=20, IdEquipo=2},
+                new Jugador{IdJugador=1, NombreJugador="Lionel Messi", Edad=24, IdEquipo=1},
+                new Jugador{IdJugador=2, NombreJugador="Hernán Crespo", Edad=21, IdEquipo=1},
+                new Jugador{IdJugador=3, NombreJugador="Gabriel Omar Batistuta", Edad=20, IdEquipo=1},
+                new Jugador{IdJugador=4, NombreJugador="Gonzalo Higuain", Edad=20, IdEquipo=1},
+                new Jugador{IdJugador=5, NombreJugador="Sergio Aguero", Edad=21, IdEquipo=1},
+                new Jugador{IdJugador=6, NombreJugador="Ariel Ortega", Edad=20, IdEquipo=1},
+                new Jugador{IdJugador=7, NombreJugador="Claudio Javier López", Edad=20, IdEquipo=1},
+                new Jugador{IdJugador=8, NombreJugador="Juan Román Riquelme", Edad=24, IdEquipo=1},
+                new Jugador{IdJugador=9, NombreJugador="Oreste Corbatta", Edad=22, IdEquipo=1},
+                new Jugador{IdJugador=10, NombreJugador="Juan Sebastián Vero", Edad=23, IdEquipo=1},
+
+                new Jugador{IdJugador=11, NombreJugador="Joaquín Botero", Edad=24, IdEquipo=2},
+                new Jugador{IdJugador=12, NombreJugador="Victor Agustín Ugarte", Edad=21, IdEquipo=2},
+                new Jugador{IdJugador=13, NombreJugador="Erwin Sánchez", Edad=20, IdEquipo=2},
+                new Jugador{IdJugador=14, NombreJugador="Marcelo Martins", Edad=20, IdEquipo=2},
+                new Jugador{IdJugador=15, NombreJugador="Julio Cesar Baldivieso", Edad=21, IdEquipo=2},
+                new Jugador{IdJugador=16, NombreJugador="Carlos Aragones", Edad=20, IdEquipo=2},
+                new Jugador{IdJugador=17, NombreJugador="Maximo Alcocer", Edad=20, IdEquipo=2},
+                new Jugador{IdJugador=18, NombreJugador="Marco Antonio Etcheverry", Edad=24, IdEquipo=2},
+                new Jugador{IdJugador=19, NombreJugador="Miguel Aguilar", Edad=22, IdEquipo=2},
+                new Jugador{IdJugador=20, NombreJugador="Juan Carlos Arce", Edad=23, IdEquipo=2},
             };
             foreach (var j in jugadores)
             {
