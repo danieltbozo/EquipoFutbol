4a64d4ca5c30373113ca7d61fa30e949c5167342
danieltbozo <38674997+danieltbozo@users.noreply.github.com>
2018-05-23 21:52:18 -0400

    Update Jugador.cs
    removido linea para no generar ids en DB

--- EquipoFutbol1/EquipoFutbol1/Models/Jugador.cs
+++ EquipoFutbol1/EquipoFutbol1/Models/Jugador.cs
@@ -8,15 +8,14 @@ using System.Threading.Tasks;
 namespace EquipoFutbol1.Models
 {
     public class Jugador
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    {  
         [Key]
         public int IdJugador { get; set; }
         public string NombreJugador { get; set; }
         public int Edad { get; set; }
-
-        [ForeignKey("IdEquipo")]
+        
         public virtual Equipo Equipo { get; set; }
+        [ForeignKey("IdEquipo")]
         public int IdEquipo { get; set; }
 
         public virtual List<Equipo> Equipos { get; set; }
